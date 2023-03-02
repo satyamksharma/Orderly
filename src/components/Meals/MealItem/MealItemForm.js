ad2c0019fa3ebcb21abf0550a00b75bc3f290bf6
Satyam Kumar <ksatyam433@gmail.com>
2023-03-02 23:55:00 +0530

    UI part done moving to logic....

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,27 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
 const MealItemForm = () => {
-    return <div>MealItemForm</div>;
+    return (
+        <form
+            action=''
+            className={classes.form}
+        >
+            <Input
+                label='Qty.'
+                input={{
+                    id: "amount",
+                    type: "number",
+                    min: "1",
+                    max: "7",
+                    step: "1",
+                    defaultValue: "1",
+                }}
+            />
+            <button>+ Add</button>
+        </form>
+    );
 };
 
 export default MealItemForm;
